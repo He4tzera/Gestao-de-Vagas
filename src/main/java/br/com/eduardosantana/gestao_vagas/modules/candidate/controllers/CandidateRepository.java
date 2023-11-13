007687075b14f29352aac91d3c945887c3b4d0fd
Eduardo Santana <dudu_lima.11@htomail.com>
2023-11-13 18:06:37 -0300

    Created use case

--- src/main/java/br/com/eduardosantana/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
+++ src/main/java/br/com/eduardosantana/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
@@ -1,12 +1,15 @@
 package br.com.eduardosantana.gestao_vagas.modules.candidate.controllers;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.eduardosantana.gestao_vagas.modules.candidate.CandidateEntity;
 
-public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
 
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
     
 } 
